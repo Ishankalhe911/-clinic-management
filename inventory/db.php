01259e1e158698f7dd0d33d77a9666a440d62844
Ishankalhe911 <thegamechangerik@gmail.com>
2025-07-20 23:15:55 -0700

    Update db.php

--- inventory/db.php
+++ inventory/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "inventory_db");
+$conn = new mysqli("localhost", "root", "", "clinic_db");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
