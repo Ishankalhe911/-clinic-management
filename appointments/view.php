7714f995cac01157afa00f37c97b406a8f811c24
Ishankalhe911 <thegamechangerik@gmail.com>
2025-07-22 22:06:26 +0530

    Update view.php

--- appointments/view.php
+++ appointments/view.php
@@ -1,45 +1,40 @@
 <?php
 include 'datafunction.php';
-if (isset($_POST['save'])) {
-    $username = $_POST['username'];
-    $passwords = $_POST['passwords'];
-    $query = "SELECT * FROM Receptionist WHERE username='$username' AND passwords='$passwords' ";
-    $query_run = mysqli_query($conn, $query);
-    if (mysqli_num_rows($query_run) > 0) {
-?>
-        <?php
-        include 'datafunction.php';
+header('Content-Type: application/json');
 
+// Allow only POST requests
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'] ?? '';
+    $passwords = $_POST['passwords'] ?? '';
+
+    // Prevent SQL Injection using prepared statements
+    $stmt = $conn->prepare("SELECT * FROM Receptionist WHERE username=? AND passwords=?");
+    $stmt->bind_param("ss", $username, $passwords);
+    $stmt->execute();
+    $resultLogin = $stmt->get_result();
+
+    // If receptionist exists
+    if ($resultLogin->num_rows > 0) {
+        $appointments = [];
         $result = mysqli_query($conn, "SELECT * FROM appointments");
-        ?>
 
-        <table border="1">
-            <link href="tableres.css" rel="stylesheet">
-            <tr>
-                <th>ID</th>
-                <th>Patient Name</th>
-                <th>Doctor Name</th>
-                <th>Date</th>
-                <th>Time</th>
-                <th>Actions</th>
-            </tr>
-
-            <?php while ($row = mysqli_fetch_assoc($result)) { ?>
-                <tr>
-                    <td><?= $row['id'] ?></td>
-                    <td><?= $row['patient_name'] ?></td>
-                    <td><?= $row['doctor'] ?></td>
-                    <td><?= $row['date_a'] ?></td>
-                    <td><?= $row['time_a'] ?></td>
-                    <td>
-                        <a href="edit.php?id=<?= $row['id'] ?>">Edit</a> |
-                        <a href="delete.php?id=<?= $row['id'] ?>" onclick="return confirm('Are you sure you want to delete this appointment?');">Delete</a>
-                    </td>
-                </tr>
-    <?php }
-        } else {
-            echo "<b>invalid username or password</b>";
+        while ($row = mysqli_fetch_assoc($result)) {
+            $appointments[] = $row;
         }
+
+        echo json_encode([
+            "status" => "success",
+            "data" => $appointments
+        ]);
+    } else {
+        echo json_encode([
+            "status" => "error",
+            "message" => "Invalid username or password"
+        ]);
     }
-    ?>
-        </table>
+} else {
+    echo json_encode([
+        "status" => "error",
+        "message" => "Only POST method allowed"
+    ]);
+}
