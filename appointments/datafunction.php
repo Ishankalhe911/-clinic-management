cbd5b7ec91d7807a9af068810d2af06adae56c92
Ishankalhe911 <thegamechangerik@gmail.com>
2025-07-22 22:24:35 +0530

    Update datafunction.php

--- appointments/datafunction.php
+++ appointments/datafunction.php
@@ -2,5 +2,11 @@
 $server_name = "localhost";
 $username = "root";
 $password = "";
-$database_name = "Patient_details";
+$database_name = "clinic_db";
+
 $conn = mysqli_connect($server_name, $username, $password, $database_name);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
